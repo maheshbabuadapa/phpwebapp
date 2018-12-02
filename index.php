e64dbedd93a5d353fda324754fe1aeb9d6cc6eea
maheshbabuadapa <root@ip-172-31-41-222.ec2.internal>
2018-12-02 05:46:49 +0000

    php-login-app

---
+++ index.php
@@ -0,0 +1,137 @@
+<?php require('includes/config.php');
+
+//if logged in redirect to members page
+if( $user->is_logged_in() ){ header('Location: memberpage.php'); exit(); }
+
+//if form has been submitted process it
+if(isset($_POST['submit'])){
+
+    if (!isset($_POST['username'])) $error[] = "Please fill out all fields";
+    if (!isset($_POST['email'])) $error[] = "Please fill out all fields";
+    if (!isset($_POST['password'])) $error[] = "Please fill out all fields";
+
+	$username = $_POST['username'];
+
+	//very basic validation
+	if(!$user->isValidUsername($username)){
+		$error[] = 'Usernames must be at least 3 Alphanumeric characters';
+	} else {
+		$stmt = $db->prepare('SELECT username FROM members WHERE username = :username');
+		$stmt->execute(array(':username' => $username));
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+		if(!empty($row['username'])){
+			$error[] = 'Username provided is already in use.';
+		}
+
+	}
+
+	if(strlen($_POST['password']) < 3){
+		$error[] = 'Password is too short.';
+	}
+
+	if(strlen($_POST['passwordConfirm']) < 3){
+		$error[] = 'Confirm password is too short.';
+	}
+
+	if($_POST['password'] != $_POST['passwordConfirm']){
+		$error[] = 'Passwords do not match.';
+	}
+	
+
+
+	//if no errors have been created carry on
+	if(!isset($error)){
+
+		//hash the password
+		$hashedpassword = $user->password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+		//create the activasion code
+		$activasion = md5(uniqid(rand(),true));
+
+		try {
+
+			//insert into database with a prepared statement
+			$stmt = $db->prepare('INSERT INTO members (username,password) VALUES (:username, :password)');
+			$stmt->execute(array(
+				':username' => $username,
+				':password' => $hashedpassword
+			));
+			$id = $db->lastInsertId('memberID');
+
+			//redirect to index page
+			header('Location: index.php?action=joined');
+			exit;
+
+		//else catch the exception and show the error.
+		} catch(PDOException $e) {
+		    $error[] = $e->getMessage();
+		}
+
+	}
+
+}
+
+//define page title
+$title = 'Demo';
+
+//include header template
+require('layout/header.php');
+?>
+
+
+<div class="container">
+
+	<div class="row">
+
+	    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+			<form role="form" method="post" action="" autocomplete="off">
+				<h2>Please Sign Up</h2>
+				<p>Already a member? <a href='login.php'>Login</a></p>
+				<hr>
+
+				<?php
+				//check for any errors
+				if(isset($error)){
+					foreach($error as $error){
+						echo '<p class="bg-danger">'.$error.'</p>';
+					}
+				}
+
+				//if action is joined show sucess
+				if(isset($_GET['action']) && $_GET['action'] == 'joined'){
+					echo "<h2 class='bg-success'>Registration successful, please check your email to activate your account.</h2>";
+				}
+				?>
+
+				<div class="form-group">
+					<input type="text" name="username" id="username" class="form-control input-lg" placeholder="User Name" value="<?php if(isset($error)){ echo htmlspecialchars($_POST['username'], ENT_QUOTES); } ?>" tabindex="1">
+				</div>
+
+				<div class="row">
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<div class="form-group">
+							<input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3">
+						</div>
+					</div>
+					<div class="col-xs-6 col-sm-6 col-md-6">
+						<div class="form-group">
+							<input type="password" name="passwordConfirm" id="passwordConfirm" class="form-control input-lg" placeholder="Confirm Password" tabindex="4">
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-xs-6 col-md-6"><input type="submit" name="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="5"></div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+</div>
+
+<?php
+//include header template
+require('layout/footer.php');
+?>
+
